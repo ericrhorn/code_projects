1104f533b7ea13216dfce72f11539ea20670abab
Eric Horn <hornericr@me.com>
2022-02-23 23:35:28 -0800

    profile site

---
+++ react/profile/client/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import './App.css';
+import ParticlesPage from './components/particles';
+// import Main from "./views/main";
+
+function App() {
+  return (
+    <div>
+      {/* <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}>
+      </div> */}
+      <ParticlesPage/>
+      {/* <Main/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react/profile/client/src/views/main.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import "../style.css";
+
+const Main = () => {
+    
+    return (
+        <div class = "name">
+            <h1>Eric Horn</h1>
+        </div>
+      
+    )
+}
+export default Main;
