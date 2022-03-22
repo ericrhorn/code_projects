0e61a612be7263fb95ca9a0668fd5ede9d0575a2
Eric Horn <hornericr@me.com>
2022-03-21 20:59:38 -0700

    js

--- javaScript.js
+++ javaScript.js
@@ -499,16 +499,6 @@ function sigma(num){
 
 }
 
-function sigma(num){
-    var sum=[];
-  for(var i=1; i<=num; i++){
-    if(sum==sum+i);
-    sum.push(i);
-  }
-  return sum;
-}
-console.log(sigma(5));
-
 
 function sigma(num) {
     var sum = 0;
@@ -525,31 +515,197 @@ sigma(3);
 sigma(2);
 
 
+function sigma(num){
+    var sum=[];
+  for(var i=1; i<=num; i++){
+    if(sum==sum+i);
+    sum.push(i);
+  }
+  return sum;
+}
+console.log(sigma(5));
+
 // Factorial - Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to the given number (inclusive).  For example, factorial(3) = 6 (or 1*2*3); factorial(5) = 120 (or 1*2*3*4*5).
 
+function factorial(num) {
+    var sum = 1;
+    var product = 1;
+    for (var i = 1; i <= num; i++) {
+      sum *= i;
+      product *= i;
+    }
+    console.log(sum);
+    return sum;
+  
+    return product;
+  }
+  factorial(3);
+  factorial(5);
+  
+  console.log(factorial(3));
+  console.log(factorial(5));
 
+
+  function factorial(num){
+    var Sum=1
+    for (var i=1; i<=num; i++){
+      Sum=Sum*i;
+    }
+    return Sum;
+  }
+  console.log(factorial (5));
 
 
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8). 
 
 
+function fib(n) {
+  
+    var a = 0, b = 1, sum = 1;
+    
+    for(var i = 2; i <= n; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    
+    return sum;
+  }
+  console.log(fib(6))
+  console.log(fib(7))
+  console.log(fib(8))
+  console.log(fib(9))
+  console.log(fib(10))
 
+
+  function fib(num) {
+    var n = 0;
+    var n1 = 1
+    var n2 = 1;
+    for(var i = 1; i <= num; i++) {
+        n2 = n + n1;
+        n = n1;
+        n1 = n2;
+      console.log(n);
+    }
+    return n2;
+}
+console.log(fib(8));
 
 
 // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
 
 
+function SecToLast (arr){
+    if (arr.length<2){
+      return "null";
+      return null;
+    } else{
+    return arr[arr.length-2];
+    }
+  }
+  console.log(SecToLast([42, true, 4, "Liam", 7]));
+  console.log(SecToLast ([4]));
 
+  function SecToLast (arr){
+    if (arr.length<2){
+      return "null";
+    } else{
+    return arr[arr.length-2];
+    }
+  }
+  console.log(SecToLast([42, true, 4, "Liam", 7]));
+  console.log(SecToLast ([4]));
 
 
 // Array: Nth-to-Last: Return the element that is N-from-array's-end.  Given ([5,2,3,6,4,9,7],3), return 4.  If the array is too short, return null.
 
+function NthtoLast(arr,n){
+    if(arr.length<n){
+      return "null";
+      return null;
+    } else {
+    return arr[arr.length-n];
+    }
+  }
+  console.log(NthtoLast ([1,3],3))
+  console.log(NthtoLast ([5,2,3,6,4,9,7],3));
+  function secMax(arr){
+ 
 
 
 
 // Array: Second-Largest: Return the second-largest element of an array. Given [42,1,4,3.14,7], return 7.  If the array is too short, return null.
 
+var max = arr[0];
+var sec = arr[0];
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]>max){
+      if(arr.length<2){
+        return null;
+      }
+        else if(arr[i]>max){
+            sec= max;
+          max = arr[i];
+        }
+      else if(arr[i]>sec && arr[i] < max)
+        sec = arr[i];
+    }
+  return sec;
+  
+}
+console.log(secMax([1, 9, 5, 4]));
+console.log(secMax([2, 10, 15, 4]));
+console.log(secMax([1]));
+console.log(secMax([]));
 
 
+function secMax(arr){
+    var max = arr[0];
+    var sec = arr[0];
+        for(var i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                sec= max;
+              max = arr[i];
+            }
+          else if(arr[i]>sec && arr[i] < max)
+            sec = arr[i];
+        }
+      return sec;
+      
+    }
+    console.log(secMax([1, 9, 5, 4]));
+    console.log(secMax([2, 10, 15, 4]));
 
 // Double Trouble: Create a function that changes a given array to list each existing element twice, retaining original order.  Convert [4, "Ulysses", 42, false] to [4,4, "Ulysses", "Ulysses", 42, 42, false, false].
+
+function doubleTrouble(arr){
+
+    var dub=[];
+    for(var i=0; i< arr.length; i++){
+      dub.push(arr[i]);
+      dub.push(arr[i]);
+    }
+   
+    return dub;
+    }
+   console.log(doubleTrouble([4,'ulysses',42,false]))
+   console.log(doubleTrouble([0,9,2,7]))
+   example.splice(1, 0, 4); 
+   example.splice(3,0,'ulysses');
+   example.splice(5,0, 42);
+   example.splice(6,0,false);
+   console.log(example)
+
+
+   function repeatArray(){
+    var arr=[4,'ulysses',42,false];
+    var newarr=[];
+    for(var i=0; i< arr.length; i++){
+      newarr.push(arr[i]);
+      newarr.push(arr[i]);
+    }
+     arr=newarr;
+    console.log(arr);
+    }
+   repeatArray();
