37d96e5d387062566fd7352b63bf4fda0f474a03
Eric Horn <hornericr@me.com>
2022-03-23 14:48:22 -0700

    api update

--- react/market_planner/client/src/components/market_info.js
+++ react/market_planner/client/src/components/market_info.js
@@ -1,102 +1,167 @@
 import React, { useEffect, useState } from "react";
 import "../style.css";
 import TableRow from "./tableRow";
-
+import axios from "axios";
 
 
 const MarketInfo = () => {
 
-    const [rowData, setRowData] = useState([])
+    // const [rowData, setRowData] = useState([])
 
-    const addRow = () => {
-        const rowInput={
-            ticker: "",
-            ammountOfShares: "",
-            currentPrice: "",
-            weekHigh: "",
-            weekLow: "",
-            dividendPerShare: "",
-            dividendPerQuarter:"",
-            dividendPerYear: "",
-            action: "",
+    // const addRow = () => {
+    //     const rowInput={
+    //         ticker: "",
+    //         ammountOfShares: "",
+    //         currentPrice: "",
+    //         weekHigh: "",
+    //         weekLow: "",
+    //         dividendPerShare: "",
+    //         dividendPerQuarter:"",
+    //         dividendPerYear: "",
+    //         action: "",
+    //     }
+    //     setRowData([...rowData, rowInput])
+    // }
+
+    // const deleteRow = (e) => {
+    //     const row = [...rowData];
+    //     row.splice(e, 1);
+    //     setRowData(row);
+    // }
+
+    // const handleChange = (e) => {
+    //     let newRowData = {...rowData};
+    //     newRowData[e.target.name] = e.target.value;
+    //     console.log(e.target.name, e.target.value);
+    //     setRowData(newRowData);
+    // };
+
+    const [stockData, setStockData] = useState([]); 
+
+    // useEffect(() => {
+    //     axios
+    //         .get('https://yh-finance.p.rapidapi.com/stock/v2/get-summary',{
+    //             params: {symbol: 'AMRN', region: 'US'},
+    //             headers: {
+    //                 'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+    //                 'X-RapidAPI-Key': '1b1c044ecdmsh43350913208335ep13f42djsn66d781d4af68',
+    //         }})
+    //         .then((response) => {
+    //             setStockData(response.data);
+    //         })
+    //         .catch((error) => {
+    //         console.log(error);
+    //         });
+    // }, []);
+
+    const options = {
+        method: 'GET',
+        url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary?limit=807',
+        headers: {
+          'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+          'X-RapidAPI-Key': '1b1c044ecdmsh43350913208335ep13f42djsn66d781d4af68'
         }
-        setRowData([...rowData, rowInput])
-    }
+      };
+      
+      axios.request(options).then(function (response) {
+          setStockData(response.data.results)
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
 
-    const deleteRow = (e) => {
-        const row = [...rowData];
-        row.splice(e, 1);
-        setRowData(row);
-    }
+    const [pokemon, setPokemon] = useState([]);
 
-    const handleChange = (e) => {
-        let newRowData = {...rowData};
-        newRowData[e.target.name] = e.target.value;
-        console.log(e.target.name, e.target.value);
-        setRowData(newRowData);
-    };
+    useEffect(() => {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        .then((response) => {
+          setPokemon(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []);
 
 
     return(
+
         <div>
-            <div class="title">
-                <h1>Check Your Investments!</h1>
-            </div>
-            <div class ="submit">
-                <button>Run Info</button>
-            </div>
-            
             <div>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Ticker</th>
-                        <th>Ammount of Shares</th>
-                        <th>Current Price</th>
-                        <th>52 Week High</th>
-                        <th>52 Weel Low</th>
-                        <th>Dividend Per Share</th>
-                        <th>Dividend Per Quarter</th>
-                        <th>Dividend Per Year</th>
-                        {/* <th>Action</th> */}
-                    </tr>
-                </thead>
-                <tbody>
-                    <TableRow rowData={rowData} handleChange={handleChange}/>
-                
-                {/* <tr>
-                    <td>
-                        <select>
-                            <option>Ticker</option>
-                        </select>
-                    </td>
-                    <td>
-                        <form>
-                            <label for="shares"></label>
-                            <input type="text" id="shares" name="shares"/>
-                        </form>
-                    </td>
-                    <td>filler info</td>
-                    <td>filler info</td>
-                    <td>filler info</td>
-                    <td>filler info</td>
-                    <td>filler info</td>
-                    <td>filler info</td>
-                    <td>
-                        <button>delete</button>
-                        <button>edit</button>
-                    </td>
-                </tr> */}
-                </tbody>
-            </table>
+                <select>
+                    {stockData.map((stockData, index) => (
+                        <option key={index}>{stockData.symbol}</option>
+                ))}
+                </select>
             </div>
-            
-            <div class = "rowButton">
-                <button onClick = {addRow}>Add Row </button>
-                <button onClick = {deleteRow}>Delete Row </button>
+            <div>
+                <select>
+                    {pokemon.map((pokemon, index) => (
+                        <option key={index}>{pokemon.name}</option>
+                    ))}
+                </select>
             </div>
-            
         </div>
+        
+        // <div>
+        //     <div class="title">
+        //         <h1>Check Your Investments!</h1>
+        //     </div>
+        //     <div class ="submit">
+        //         <button>Run Info</button>
+        //     </div>
+            
+        //     <div>
+        //     <table class="table table-bordered">
+        //         <thead>
+        //             <tr>
+        //                 <th>Ticker</th>
+        //                 <th>Ammount of Shares</th>
+        //                 <th>Current Price</th>
+        //                 <th>52 Week High</th>
+        //                 <th>52 Weel Low</th>
+        //                 <th>Dividend Per Share</th>
+        //                 <th>Dividend Per Quarter</th>
+        //                 <th>Dividend Per Year</th>
+        //                 {/* <th>Action</th> */}
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             <TableRow rowData={rowData} handleChange={handleChange}/>
+                
+        //         {/* <tr>
+        //             <td>
+        //                 <select>
+        //                     <option>Ticker</option>
+        //                 </select>
+        //             </td>
+        //             <td>
+        //                 <form>
+        //                     <label for="shares"></label>
+        //                     <input type="text" id="shares" name="shares"/>
+        //                 </form>
+        //             </td>
+        //             <td>filler info</td>
+        //             <td>filler info</td>
+        //             <td>filler info</td>
+        //             <td>filler info</td>
+        //             <td>filler info</td>
+        //             <td>filler info</td>
+        //             <td>
+        //                 <button>delete</button>
+        //                 <button>edit</button>
+        //             </td>
+        //         </tr> */}
+        //         </tbody>
+        //     </table>
+        //     </div>
+            
+        //     <div class = "rowButton">
+        //         <button onClick = {addRow}>Add Row </button>
+        //         <button onClick = {deleteRow}>Delete Row </button>
+        //     </div>
+            
+        // </div>
     )
 }
 
--- react/market_planner/client/src/components/tableRow.js
+++ react/market_planner/client/src/components/tableRow.js
@@ -1,12 +1,43 @@
 import React, { useEffect, useState } from "react";
 import "../style.css";
-
+import axios from "axios";
 
 
 const TableRow = ({rowData, handleChange}) => {
 
+    const [stockData, setStockData] = useState([]); 
 
+    useEffect(() => {
+        axios
+            .get('https://yh-finance.p.rapidapi.com/stock/v2/get-summary', {
+                headers: {
+                    'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+                    'x-rapidapi-key': '1b1c044ecdmsh43350913208335ep13f42djsn66d781d4af68'
+                }
+            })
+            .then((response) => {
+                setStockData(response.data.results);
+            })
+            .catch((error) => {
+            console.log(error);
+            });
+    }, []);
 
+// const options = {
+//   method: 'GET',
+//   url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+//   params: {symbol: 'T', region: 'US'},
+//   headers: {
+//     'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+//     'x-rapidapi-key': '1b1c044ecdmsh43350913208335ep13f42djsn66d781d4af68'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 
     return(
         rowData.map((data, idx) => {
@@ -22,30 +53,48 @@ const TableRow = ({rowData, handleChange}) => {
                 // action,
             } = data;
             
+
+        //     <div className="App">
+        //     {pokemon.map((pokemon, index) => (
+        //       <div key={index}>{pokemon.name}</div>
+        //     ))}
+        //   </div>
+
+
             return (
-                <tr key={idx}>
-                    <td value={ticker} name ="ticker">
-                        <select>
-                            <option>Ticker</option>
-                        </select>
-                    </td>
-                    <td value={ammountOfShares} name="ammountOfShares">
-                        <form>
-                            <label for="ammountOfShares"></label>
-                            <input type="text" id="ammountOfShares" name="ammountOfShares"/>
-                        </form>
-                    </td>
-                    <td value={currentPrice} name ="currentPrice"></td>
-                    <td value={weekHigh} name="weekHigh"></td>
-                    <td value={weekLow} name="weekLow"></td>
-                    <td value={dividendPerShare} name="dividendPerShare"></td>
-                    <td value={dividendPerQuarter} name="dividendPerQuarter"></td>
-                    <td value={dividendPerYear} name="dividendPerYear"></td>
-                    {/* <td value={action} name = "action">
-                        <button>delete</button>
-                        <button>edit</button>
-                    </td> */}
-                </tr>
+                <div>
+                    {stockData.map((stockData, index) => (
+                            <select>
+                                <option key={index}>Ticker {stockData.symbol}</option>
+                            </select>
+                        ))}
+                </div>
+                // <tr key={idx}>
+                //     <td value={ticker} name ="ticker">
+                //         {stockData.map((stockData, index) => (
+                //             <select>
+                //                 <option key={index}>Ticker {stockData.ticker}</option>
+                //             </select>
+                //         ))}
+                        
+                //     </td>
+                //     <td value={ammountOfShares} name="ammountOfShares">
+                //         <form>
+                //             <label for="ammountOfShares"></label>
+                //             <input type="text" id="ammountOfShares" name="ammountOfShares"/>
+                //         </form>
+                //     </td>
+                //     <td value={currentPrice} name ="currentPrice"></td>
+                //     <td value={weekHigh} name="weekHigh"></td>
+                //     <td value={weekLow} name="weekLow"></td>
+                //     <td value={dividendPerShare} name="dividendPerShare"></td>
+                //     <td value={dividendPerQuarter} name="dividendPerQuarter"></td>
+                //     <td value={dividendPerYear} name="dividendPerYear"></td>
+                //     {/* <td value={action} name = "action">
+                //         <button>delete</button>
+                //         <button>edit</button>
+                //     </td> */}
+                // </tr>
             )
         })
     )
