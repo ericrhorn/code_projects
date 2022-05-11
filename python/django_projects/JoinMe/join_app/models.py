b1c5d8e9788f83cce67125bf097506fffa62321b
Eric Horn <hornericr@me.com>
2022-05-11 12:51:57 -0700

    portfolio and joinme updates

--- python/django_projects/JoinMe/join_app/models.py
+++ python/django_projects/JoinMe/join_app/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+import bcrypt
+import re
 
-# Create your models here.
+
+class UserManager(models.Manager):
+    def registration_validator(self, postData):
+        errors = {}
+        if len(postData["first_name"]) < 2:
+            errors["first_name"] = "First Name should be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "Last Name should be at least 2 characters"
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address!"
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Must be a valid email address"
+
+        logged_user = User.objects.filter(email=postData["email"])
+        if len(logged_user) > 0:
+            errors['duplicate'] = "Email is already registered"
+
+        if len(postData['password']) < 5:
+            errors["password"] = "Passwords should be at least 5 charicters"
+
+        if postData['password'] != postData ['password_confirm']:
+            errors["password_confirm"] = "Passwords must match"
+
+        return errors
+
+    def login_validator(self, postData):
+        errors = {}
+        logged_user = User.objects.filter(email=postData['email'])
+        if len(logged_user) !=1:
+            errors['email'] = 'Email is not registered'
+        if len(postData['email']) == 0:
+            errors['email'] = 'Please enter an email address'
+        if len(postData['password']) < 5:
+            errors["password"] = "Passwords should be at least 5 charicters"
+        elif bcrypt.checkpw(postData['password'].encode(), logged_user[0].password.encode())!= True:
+            errors['password_check'] = 'Invalid email or password'
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.TextField(max_length = 50)
+    password = models.TextField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+class EventManager(models.Manager):
+    def event_validator(self, postData):
+        errors = {}
+        if len(postData["event_name"]) < 3:
+            errors["event_name"] = "Event name should be at least 3 characters"
+        if len(postData["location"]) < 3:
+            errors["location"] = "Location should be at least 3 characters"
+        if len(postData["description"]) < 3:
+            errors["description"] = "Description should be at least 3 characters"
+        if len(postData["start_time"]) == 0:
+            errors["start_time"] = "Start time must be entered"
+        if len(postData["end_time"]) == 0:
+            errors["end_time"] = "end time must be entered"
+        if len(postData["price"]) == 0:
+            errors["price"] = "Price must be entered"
+        if len(postData["start_date"]) == 0:
+            errors["start_date"] = "Start date must be entered"
+        if len(postData['end_date']) == 0:
+            errors["end_date"] = "Start date must be entered"
+        return errors
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+
+    description = models.TextField(max_length=500)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    created_by = models.ForeignKey(User, related_name='event_planned', on_delete=models.CASCADE)
+    join_event = models.BooleanField(default=False)
+    objects = EventManager()
--- python/django_projects/JoinMe/join_app/views.py
+++ python/django_projects/JoinMe/join_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
-# import bcrypt
+import bcrypt
 
 from django.shortcuts import render, HttpResponse
 
@@ -36,4 +36,153 @@ def logout(request):
 def delete(request):
     return redirect('/')
 
+
+
+
+
+
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from .models import *
+# import bcrypt
+
+# def index(request):
+#     return render(request, "reglog.html")
+
+
+# def dashboard(request):
+#     if "user_id" not in request.session:
+#         return redirect ("/")
+#     else:
+#         logged_user = User.objects.filter(id=request.session['user_id'])
+#         context = {
+#             'trips': Trip.objects.all(),
+#             'user': logged_user[0],
+#             'all_users': User.objects.all(),
+#         }
+#         return render(request, "dashboard.html", context)
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         errors = User.objects.registration_validator(request.POST)
+#     if len(errors) > 0:
+#         for key, value in errors.items():
+#             messages.error(request, value)
+#         return redirect("/")
+#     else:
+#         password = request.POST['password']
+#         pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt(4)).decode()    
+#         user = User.objects.create(
+#             first_name = request.POST ["first_name"],
+#             last_name = request.POST ["last_name"],
+#             email = request.POST ["email"],
+#             password = pw_hash,
+#         )
+#         request.session['user_id'] = user.id
+#         return redirect("/dashboard")
+#     return redirect('/')
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         errors = User.objects.login_validator(request.POST)
+#         if len(errors) > 0:
+#             for key, value in errors.items():
+#                 messages.error(request, value)
+#             return redirect('/')
+#         else:
+#             logged_user = User.objects.filter(email = request.POST['email'])
+#             request.session['user_id'] = logged_user[0].id
+#         return redirect('/dashboard')
+#     return redirect("/")
+
+# def new_trip(request):
+#     if request.method == 'POST':
+#         errors = Trip.objects.trip_validator(request.POST)
+#         if len(errors) > 0:
+#             for key, value in errors.items():
+#                 messages.error(request, value)
+#             return redirect("/trip")
+#         else:
+#             user = User.objects.get(id=request.session['user_id'])
+#             trip = Trip.objects.create(
+#             destination = request.POST ["destination"],
+#             plan = request.POST ["plan"],
+#             start_date = request.POST ["start_date"],
+#             end_date = request.POST ["end_date"],
+#             planned_by = user,
+#         )
+#         return redirect('/dashboard')
+#     return redirect('/')
+
+
+# def trip(request):
+#     context = {
+#         "trip": Trip.objects.all(),
+#         "user": User.objects.get(id=request.session['user_id']),
+#     }
+#     return render(request, 'new.html', context)
+
+# def update(request, trip_id):
+#     if request.method == 'POST':
+#         errors = Trip.objects.trip_validator(request.POST)
+#         if len(errors) > 0:
+#             for key, value in errors.items():
+#                 messages.error(request, value)
+#             return redirect(f"/{ trip_id }/edit")
+#         else:
+#             update = Trip.objects.get(id=trip_id)
+#             update.destination = request.POST['destination']
+#             update.plan = request.POST['plan']
+#             update.start_date = request.POST['start_date']
+#             update.end_date = request.POST['end_date']
+#             update.save()
+#         return redirect('/dashboard')
+#     return redirect("/")
+    
+
+# def edit(request, trip_id):
+#     context = {
+#         "trip": Trip.objects.get(id=trip_id),
+#         "user": User.objects.get(id=request.session['user_id']),
+#     }
+#     return render(request, 'edit.html', context)
+
+
+# def info(request, trip_id):
+#     context = {
+#         "trip": Trip.objects.get(id=trip_id),
+#         "user": User.objects.get(id=request.session['user_id']),
+#     }
+#     return render(request, 'info.html', context)
+
+
+# def granted(request, wish_id):
+#     pass
+#     # user = Users.objects.get(id=request.session['user_id'])
+#     # wish = Wish.objects.get(id=wish_id)
+#     # wish.granted = True
+#     # wish.save()
+#     # return redirect('/dashboard')
+
+
+# def like(request, wish_id):
+#     pass
+# #     user = Users.objects.get(id=request.session['user_id'])
+# #     wish = Books.objects.get(id=book_id)
+# #     user.books_liked.add(book)
+# #     return redirect('/dashboard')
+
+
+# def delete(request, trip_id):
+#     if request.method == 'POST':
+#         delete = Trip.objects.get(id=trip_id)
+#         delete.delete()
+#         return redirect('/dashboard')
+#     return redirect('/')
+
+# def logout(request):
+#     request.session.flush()
+#     return redirect('/')
 
