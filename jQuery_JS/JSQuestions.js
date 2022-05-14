6b98f5736f47d4c309169c8a98343ecd818d3eeb
Eric Horn <hornericr@me.com>
2022-05-13 23:23:33 -0700

    join me updates

--- jQuery_JS/JSQuestions.js
+++ jQuery_JS/JSQuestions.js
@@ -154,6 +154,24 @@
 
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8). 
 
+// recursive
+function rFib( n ) {
+        if ( n < 2 ) {
+            return n;
+        }
+        return rFib( n-1 ) + rFib( n-2 );
+    }
+    rFib(20);
+    // iterative
+    function iFib( n ) {
+        const vals = [ 0, 1 ];
+        while(vals.length-1 < n) {
+            let len = vals.length;
+            vals.push( vals[len-1] + vals[len-2] );
+        }
+        return vals[n];
+    }
+    iFib(20);
 
 
 
