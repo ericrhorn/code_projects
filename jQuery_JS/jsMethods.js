7e38196069291ac5164e29d40b50831783150581
Eric Horn <hornericr@me.com>
2022-06-21 17:01:26 -0700

    leetcode

---
+++ jQuery_JS/jsMethods.js
@@ -0,0 +1,133 @@
+// .slice() -- returns elements from original array minus the elements instructed to be sliced. does not modify the original array. takes two parameters (start, end) array.slice(start, end)
+
+const numsArr = [1,2,3,4,5,6,7,8,9,10]
+var newArr = numsArr.slice(0, ...numsArr)
+console.log(newArr)
+
+
+
+// .concat() -- joins two or more arrays returning a new array and does not change the existing arr. array1.concat(array2, array3, ..., arrayX)
+
+const concatArr1 = [1,2,3,4,5], concatArr2 = [6,7,8,9,10]
+var newArr = concatArr1.concat(concatArr2)
+console.log(newArr)
+
+
+
+// .splice() -- adds and/or removes array elements. overwrites the original array. array.splice(index, howmany, item1, ....., itemX)
+
+// remove 3 from the array and print array
+const spliceArr = [1,2,3,4,5,6,7,8,9,10]
+    spliceArr.splice(2, 1)
+console.log(spliceArr)
+
+// remove 3 from the array and print 3 (the removed element)
+const spliceArr2 = [1,2,3,4,5,6,7,8,9,10]
+var newArray = spliceArr2.splice(2, 1)
+console.log(newArray)
+
+
+
+// .pop() -- removes the last element from an array
+
+const popArr = [1,2,3,4,5]
+var newArray2 = popArr.pop()
+console.log(newArray2)
+console.log(popArr)
+
+
+
+// .push() -- adds elements to the end of the array 
+
+const pushArr = [1,2,3,4,5]
+console.log(pushArr.length)
+pushArr.push(6,7,8,9,10)
+console.log(pushArr)
+console.log(pushArr.length)
+
+
+// .map() creates a new array from calling a function for every array element. calls a function once for each element in an array, does not execute the function for empty arrays, does not change the original array.
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+var newArr = numbers.map(newFunction)
+console.log(newArr)
+
+function newFunction(num){
+    return num * 10
+}
+
+const persons = [
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"}
+  ];
+  
+  var newPerson = persons.map(getFullName);
+  
+  function getFullName(item) {
+    return [item.firstname,item.lastname].join(" ");
+  }
+  console.log(persons)
+  console.log(newPerson)
+
+
+// .sort() -- sorts the elements of an array, overwrites the original array, sorts teh elements as strings in alphabetical and ascending order
+  // numerical sort requires parameters and a compare function 
+
+  const fruits = ["Banana", "Orange", "Apple", "Mango"];
+  fruits.sort();
+  console.log(fruits)
+
+
+  const points = [40, 100, 1, 5, 25, 10];
+  points.sort(function(a, b){return a-b});
+  console.log(points)
+
+
+  const points2 = [40, 100, 1, 5, 25, 10];
+  points2.sort(function(a, b){return b-a});
+  console.log(points2)
+
+// find the lowest value using sort
+
+const points3 = [40, 100, 1, 5, 25, 10];
+points3.sort((a,b) => (a-b))
+let lowest = points3[0]
+console.log(lowest)
+
+
+// find the highest value using sort
+
+const points4 = [40, 100, 1, 5, 25, 10];
+points4.sort((a,b) => (b-a))
+let highest = points4[0]
+console.log(highest)
+
+const points5 = [40, 100, 1, 5, 25, 10];
+points5.sort((a,b) => (a-b))
+let highest2 = points5[points5.length -1]
+console.log(highest2)
+
+
+
+// .join() -- returns an array as a string. does not change the original array. array.join(separator)default separator is a comma
+
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruit.join(" and ");
+console.log(text)
+
+
+// .filter()
+// removing items from an array 
+
+var arr = [1,2,3,4,5,6]
+var newArr = arr.filter(arr => arr !== 3)
+console.log(newArr)
+
+
+var arr = [1,2,3,4,5,6]
+var newArr = arr.filter(function(arr) {
+    return arr !== 3
+})
+console.log(newArr)
+
--- jQuery_JS/leetCode100.js
+++ jQuery_JS/leetCode100.js
@@ -38,3 +38,147 @@ var twoSum = function(nums, target) {
     }
 }
 console.log(twoSum([2,7,11,15], 9))
+
+
+// ----------------------------------------------------------------------------------------------------
+// top 100 leetcode EASY
+
+// valid parentheses
+
+function isValid(s){
+    var newArr = []
+    var brackets = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+    for (var x = 0; x < s.length; x++)
+}
+
+
+// merge two sorted lists
+
+function merge(one, two){
+    one = one.concat(two).sort((a,b) => (a-b))
+    return one
+}
+console.log(merge([1,2,4], [1,3,4]))
+
+var mergeTwoLists = function(list1, list2) {
+    list1 = list1.concat(list2).sort((a,b) => (a-b))
+    return list1
+};
+console.log(mergeTwoLists([1,2,4], [1,3,4]))
+
+
+var mergeTwoLists = function(list1, list2,) {
+    list1.splice(0, list2)
+    // list1.sort((a,b) => (a-b))
+    // list1 = list1.concat(list2).sort((a,b) => (a-b))
+    return list1
+};
+console.log(mergeTwoLists([1,2,4], [1,3,4]))
+
+
+
+const spliceArr = [1,2,3,4,5,6,7,8,9,10]
+    spliceArr.splice(2, 1)
+console.log(spliceArr)
+
+
+// Search insert position
+
+var searchInsert = function (nums, target){
+    var targetNum = target;
+    for (var x = 0; x < nums.length; x++){
+        // >= will return the index value if the target doesnt exist in the arr. == will only return the index if the target exists
+        if (nums[x] >= targetNum){
+            return x
+        } 
+    }
+    // will return the length which is the last index value
+    return nums.length;
+}
+console.log(searchInsert([1,3,5,6], 5))
+console.log(searchInsert([1,3,5,6], 2))
+console.log(searchInsert([1,3,5,6], 7))
+
+// better and faster search
+
+searchInsert2 = (nums, target) => {
+    var start = 0;
+    var end = nums.length;
+
+    while (start < end) {
+        const middle = Math.floor((start + end) / 2)
+
+        if (nums[middle] < target){
+            start = middle + 1;
+        }
+        else {
+            end = middle;
+        }
+    }
+    return start;
+}
+console.log(searchInsert2([1,3,5,6], 5))
+
+
+
+// Maximum subarray
+
+// Solution #1 Brute Force:
+
+// We need to iterate over the array with one for loop and we need an additional for loop to scan the elements to the right of it, with this in mind, we can accumulate the combinations of the sub array to achieve the largest sum.
+
+// The reason we need to accumulate the combinations is because we need to keep track of the locally optimal max sum sub array while simultaneously comparing it to the global max sum sub array. Also, by accumulating the numbers in the sub array we might run into negative numbers that will decrease the total value of the sum, which is why we need to always compare the local sum to the global sum.
+
+// After going through the entire array with double for loops, you should be able to return the global max sum sub array.
+
+var maxSubArray = function(nums) {
+    var maxSum = nums[0];
+    // loop through the entire array because the whole arr migh be the max sub arr
+    for (var x = 0; x < nums.length; x++){
+        var localSum = 0;
+        // y starts at x because we are compairing all conbinations of nums[x]
+        // every iteration of nums[x] localMax will reset ans the second loop y will accumulate all the combinaitons 
+        for(var y = x; y < nums.length; y++){
+            localSum += nums[y]
+            if (localSum > maxSum){
+                maxSum = localSum;
+            }
+        }
+    }
+    return maxSum
+};
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+// Solution #2 Kadane's Algorithm:
+
+// Kadane's algorithm is a combination of the greedy algorithm and dynamic programming.
+
+// Greedy algorithm:
+// We keep track of the locally optimal solution while simultaneously comparing it to our global solution.
+
+// Dynamic programming:
+// Once we solved the sub problem, in this case, the maximum sum of the sub array then we can use memoization to keep track of the solution. By keeping track of our previous solution (local solution), it allows us to solve the problem once and we can build the next solution. These overlapping sub problems plus the greedy algorithm allows us to build the solution piece by piece in O(n) time.
+
+function maxSubArray2(nums){
+    var maxSum = 0;
+    var localSum = 0;
+    // start the loop at index 1 because maxSum + localMax has already been solved 
+    // now we can take the previous soloution and apply it to index 1
+    for (var x = 1; x < nums.length; x++){
+        // we keep track of the prev soloution (localMax[x-1] and memorize it by storing it in localMax)
+        localSum = Math.max(nums[x], localSum + nums[x]);
+        // Greedy algorithm, compait local soloution to the global soloution
+        maxSum = Math.max(localSum, maxSum);
+    }
+    return maxSum
+}
+console.log(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]))
+
+
+
+// climbing stairs
+
