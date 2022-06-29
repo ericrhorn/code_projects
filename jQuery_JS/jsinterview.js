0d73ac9b66510c8128f66097fb2b0ea4d5503d70
Eric Horn <hornericr@me.com>
2022-06-29 10:26:12 -0700

    js interview practice

--- jQuery_JS/jsinterview.js
+++ jQuery_JS/jsinterview.js
@@ -1,3 +1,48 @@
+// ziggzagg
+
+function solution(numbers) {
+  var index = 0
+  var newArr = [];
+  
+  for (var x = 0; x < numbers.length-2; x++){
+      if ((numbers[x] < numbers[x+1] && numbers[x+1] > numbers[x+2]) || 
+          (numbers[x] > numbers[x+1] && numbers[x+1] < numbers[x+2])) {
+          newArr[index++]=1;
+      }
+      else {
+        newArr[index++]=0;
+      }
+  }
+  return newArr;
+}
+console.log(solution([1,2,1,3,4]))
+
+
+
+// lexicographical order
+
+
+
+// You are given an array of arrays a. Your task is to group the arrays a[i] by their mean values, so that arrays with equal mean values are in the same group, and arrays with different mean values are in different groups.
+// Each group should contain a set of indices (i, j, etc), such that the corresponding arrays (a[i], a[j], etc) all have the same mean. Return the set of groups as an array of arrays, where the indices within each group are sorted in ascending order, and the groups are sorted in ascending order of their minimum element.
+
+
+
+
+// Given an array of positive integers a, your task is to calculate the sum of every possible a[i] ∘ a[j], where a[i] ∘ a[j] is the concatenation of the string representations of a[i] and a[j] respectively.
+
+function concatSum(a){
+  var sum = 0;
+
+  for (var x=0; x<a.length; x++){
+    for (var y=0; y<a.length; y++){
+      sum += Number("" + a[x] + a[y])
+    }
+  }
+  return sum;
+}
+console.log(concatSum([10,2]))
+
 function findTwoSum(arr, sum) {
     // Your code goes here
     var index = [];
