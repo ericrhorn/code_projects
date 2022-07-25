1b86cce55af64542590c198a5cc75ad1071396a5
Eric Horn <hornericr@me.com>
2022-07-25 10:23:57 -0700

    css

--- python/django_projects/JoinMe/JoinMe/urls.py
+++ python/django_projects/JoinMe/JoinMe/urls.py
@@ -24,7 +24,10 @@ Including another URLconf
 #     path('admin/', admin.sites.urls)         # comment out, or just delete
 # ]
 
+from xml.dom.minidom import Document
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 # from join_app.views import (
@@ -38,3 +41,7 @@ urlpatterns = [
     path('', include('user_app.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
